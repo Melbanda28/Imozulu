526074131270894cf456d18a978b7643380ee25a
Melbanda28 <122828898+Melbanda28@users.noreply.github.com>
2024-05-29 10:26:10 +0000

    updated my js to give current city and temperature

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-function updateWeather(response) {}
+function updateWeather(response) {
+  console.log(response.data);
+  let temperature = Math.round(response.data.temperature.current);
+  let currentTemperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#weather-app-city");
+  currentTemperatureElement.innerHTML = temperature;
+  cityElement.innerHTML = response.data.city;
+}
 
 function searchCity(city) {
-  let apiKey = ` 0a266418598ob604ae10378et2402a5f`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key={ 0a266418598ob604ae10378et2402a5f}`;
+  let apiKey = "0a266418598ob604ae10378et2402a5f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   console.log(apiUrl);
   axios.get(apiUrl).then(updateWeather);
 }
