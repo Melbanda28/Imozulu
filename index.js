8f04b9f6e04f59888c16d653cfe66c66fa25b224
Melbanda28 <122828898+Melbanda28@users.noreply.github.com>
2024-06-06 19:07:49 +0000

    formatted the dates to show  a 5 day forecast

--- index.js
+++ index.js
@@ -65,6 +65,13 @@ function searchWeatherCity(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "0a266418598ob604ae10378et2402a5f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -75,7 +82,7 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Wed", "Thur", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
@@ -84,7 +91,7 @@ function displayForecast(response) {
         forecastHtml +
         `
     <div class = "weather-forecast-day">
-    <div class = "weather-forecast-date">Tue</div>
+    <div class = "weather-forecast-date">${formatDay(day.time)}</div>
     <img src="${day.condition.icon_url}" id = "weather-forecast-icon" />
     <div class ="weather-forecast-temperatures">
       <div class =" weather-forecast-temperature">
